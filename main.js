b0492ec78004cf3d88905f2eec359557d419e333
任妍 <39343739+vvvvvic@users.noreply.github.com>
2021-12-14 16:53:23 +0800

    创建项目

---
+++ main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron')
+const isDev = require('electron-is-dev')
+
+let mainWindow
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 680,
+    webPreferences: {
+      // nodeIntegration true 可以使用nodejs api
+      nodeIntegration: true,
+      nodeIntegrationInWorker: true,
+      nodeIntegrationInSubFrames: true,
+      // 如果想在渲染器中使用nodejs， 上面三个写成true， 下面一个写成false
+      contextIsolation: false,
+      enableRemoteModule: true,  // 使用remote模块
+      webviewTag: true
+    }
+  })
+  const urlLocation = isDev ? 'http://localhost:3000' : ''
+  mainWindow.loadURL(urlLocation)
+})
