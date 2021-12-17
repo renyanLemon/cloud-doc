f41db878461283d12d4ee4408523f8cec695f175
任妍 <39343739+vvvvvic@users.noreply.github.com>
2021-12-17 18:06:22 +0800

    feat:增加组件类型检查和默认值

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { FileSearch } from './components/FileSearch'
 
 function App() {
   return (
     <div className="App container-fluid">
       <div className='row'>
-        <div className='col-3 bg-danger left-panel'>
-          <h1>this is the left</h1>
+        <div className='col-4 bg-info left-panel'>
+          <FileSearch
+          title='我的云文档'
+          onFileSearch={(value) => {
+            console.log(1111,value)
+          }}>
+          </FileSearch>
         </div>
-        <div className='col-9 bg-primary right-panel'>
+        <div className='col-8 bg-light right-panel'>
         <h1>this is the right</h1>
         </div>
       </div>
